d05abbe3d5dbaa25e4487bf2c528cce778423a57
blry12 <bradl79@comcast.net>
2024-05-13 21:25:05 -0500

    fix
    fix

--- repo/plugin.program.auraflix.omega/uservar.py
+++ repo/plugin.program.auraflix.omega/uservar.py
@@ -1,10 +1,10 @@
 import xbmcaddon
 
 '''#####-----Build File-----#####'''
-buildfile = 'http://auraflix.io/auraflix/texts/Omega_Builds.xml'
+buildfile = 'https://auraflix.io/auraflix/texts/Omega_Builds.xml'
 
 '''#####-----Notifications File-----#####'''
-notify_url  = 'http://auraflix.io/auraflix/texts/wizard_notify_omega.txt'
+notify_url  = 'https://auraflix.io/auraflix/texts/wizard_notify_omega.txt'
 
 '''#####-----Excludes-----#####'''
 excludes  = ['plugin.video.whatever']
